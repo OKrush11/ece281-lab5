5526cfd7efc5bc7b7ca2f56288a0571e20a58e8d
OKrush11 <253545513+OKrush11@users.noreply.github.com>
2026-05-08 12:30:51 -0600

    added overflow and fixed carry?

--- src/ALU.vhd
+++ src/ALU.vhd
@@ -96,11 +96,11 @@ begin
                    
       o_result <= w_result;
       
-        w_flags <= "1000" when w_result(7) = '1' else
-                   "0100" when w_result = "00000000" else
-                   "0010" when ((w_carryoadd = '1' and i_op = "000") or (w_carryosub = '1' and i_op = "001"))else
-                   "0000";
-      o_flags <= w_flags;
+        o_flags(3) <= '1' when w_result(7) = '1' else '0';
+        O_flags(2) <= '1' when w_result = "00000000" else '0';
+        o_flags(1) <= '1' when ((w_carryoadd = '1' and i_op = "000") or (w_carryosub = '1' and i_op = "001"))else
+                       '0';
+        o_flags(0) <= ((not w_result(7)) and w_reg1(7) and w_reg2(7)) or (w_result(7) and (not w_reg1(7)) and (not w_reg2(7))) when i_op = "000" else '0';
       
       
 end Behavioral;
