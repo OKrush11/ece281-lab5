a2abd1b11cea9decb52f380427efec94b7772745
OKrush11 <253545513+OKrush11@users.noreply.github.com>
2026-05-08 17:51:51 -0600

    bruv

--- src/ALU.vhd
+++ src/ALU.vhd
@@ -101,6 +101,6 @@ begin
         O_flags(2) <= '1' when w_result = "00000000" else '0';
         o_flags(1) <= '1' when (((w_carryoadd = '1' and i_op = "000") or ((w_carryosub) = '1' and i_op = "001")) or w_result = "00000111")  else
                      '0';
-        o_flags(0) <= (w_result(7) and (not w_reg1(7)) and (not w_reg2(7)));
+        o_flags(0) <= (w_result(7) and (not w_reg1(7)) and (not w_reg2(7))) when i_op = "000" else '0';
       
 end Behavioral;
